d9852b5f781ad75cb746f9c9228d77e5ba3ce888
Patryk Jakubiak <jaqbiak@MacBook-Pro-Patryk.local>
2020-12-08 01:22:28 +0100

    Initial commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Patryk Jakubiak on 08/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
